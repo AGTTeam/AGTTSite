0a025c6fc176cdac365021f89bc84630bd90d20e
Illidan <illidanrulez@gmail.com>
2024-08-10 17:46:32 +0200

    Added guide-conditional component.

---
+++ components/content/GuideConditional.vue
@@ -0,0 +1,26 @@
+<script setup>
+const route = useRoute()
+import ALL_PATCH_DATA from '/assets/patch-data.json';
+const PATCH_DATA = ALL_PATCH_DATA[route.params.game];
+const props = defineProps({
+    platform: {
+        type: String,
+        required: false,
+        default: ""
+    },
+    cheats: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <div v-if="platform != '' && route.params.platform == platform">
+        <slot />
+    </div>
+    <div v-if="cheats && PATCH_DATA.platforms[route.params.platform].cheats != undefined">
+        <slot />
+    </div>
+</template>
--- content/game/resonance/patch/en.md
+++ content/game/resonance/patch/en.md
@@ -7,6 +7,8 @@ locale: en
 ## Rom Patcher
 Please select your dumped _Soul Eater: Battle Resonance_ `.iso` ROM file, choose the options you'd like to use, then click "Patch ROM" to apply the patch.
 
+
+::guide-conditional{platform="psp"}
 ### Compatibility Notes
 ::iconified-text
 ---
@@ -16,4 +18,5 @@ unwrap: true
 When emulating the game, you may experience some visual errors (rips, lines etc), if the internal rendering resolution is set too high. The ON/OFF Autosave graphics under the Options/Save Load may also be missing.
 <br /><br />
 Both of these issues are present in the unpatched game and do not happen on real hardware.
+::
 ::
