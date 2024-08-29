a25ca46d18dfb512511529483158a32cce108c3e
Illidan <illidanrulez@gmail.com>
2024-08-29 12:06:27 +0200

    Fixed page title.

--- components/content/GuideBuy.vue
+++ components/content/GuideBuy.vue
@@ -4,5 +4,5 @@ const route = useRoute()
 </script>
 
 <template>
-    <ContentDoc :path="`/game/${route.params.game}/buy/${locale}`" />
+    <ContentDoc :path="`/game/${route.params.game}/buy/${locale}`" :head="false" />
 </template>
