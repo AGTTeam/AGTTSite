3d86e46136f210a47b07fbb54e677db7bf6512c0
Illidan <illidanrulez@gmail.com>
2024-09-09 20:02:01 +0200

    Tweak logo size.

--- layouts/game.vue
+++ layouts/game.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="content-body">
-        <Navbar :showHome=true />
+        <div id="top-navbar">
+            <Navbar :showHome=true />
+        </div>
         <slot />
         <Footer />
     </div>
@@ -11,7 +13,12 @@
 #content-body {
     display: flex;
     flex-direction: column;
+    align-items: center;
+    width: 100%;
     min-height: 100vh;
     background: linear-gradient(180deg, rgba(180, 213, 191, 1) 0%, rgba(255, 255, 255, 0.01) 9em);
+}
+#top-navbar {
+    width: 90vw;
 }
 </style>
