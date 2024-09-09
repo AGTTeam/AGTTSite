65eacb26f7c97deed6f18eeffafa2eb80ef8872f
PhantomandGhost <54214467+PhantomandGhost@users.noreply.github.com>
2024-09-09 20:09:50 +0100

    Attempting to fix bg jumping on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,8 @@
     <div class="page">
         <div class="content">
             <Navbar />
+		<div class="body-bg">
+		</div>	
             <slot />
         </div>
         <Footer />
@@ -16,10 +18,7 @@
     flex-direction: column;
     align-items: center;
     width: 100%;
-    min-height: 100lvh;
-    background-attachment: fixed;
-    background-image: url('/images/background.png');
-    background-size: cover;
+    min-height: 100vh;
 }
 
 .page .content {
@@ -28,5 +27,21 @@
     justify-content: center;
     max-width: 1200px;
     width: 90vw;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.body-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('/images/background.png');
+  background-size: cover;
+  z-index: -1; /* Make sure the background stays behind other content */
 }
 </style>
